b59925af922bf7e8f3de96110d6f071a5175e56c
joe-oli <joeSydneyAu@gmail.com>
2024-03-15 17:43:46 +1100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css' //either in here, or 1 level above in index.tsx
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="App-navbar">
+        {/* Add your menu items here */}
+        <a href="#">Menu Item 1</a>
+        <a href="#">Menu Item 2</a>
+        <a href="#">Menu Item 3</a>
+      </nav>
+      <main className="App-body">
+        {/* Add your main content here */}
+        <p>Welcome to my app!</p>
+      </main>
     </div>
   );
 }
